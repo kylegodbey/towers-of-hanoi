c6f8fea5b2aa76da9c897a00414e54e64b7bfb0b
Kyle Godbey <kylegodbey@gmail.com>
2015-02-24 01:21:03 +0000

    finished

--- towers.pl
+++ towers.pl
@@ -1,6 +1,14 @@
-printing(X,Y):-
-  write('Hello World.'),
-  nl,  
-  write(X),
-  nl,
-  write(Y).
+play(1,I,J,_):-
+  write('Place disk from '),
+  write(I),
+  write(' peg to the '),
+  write(J),
+  write(' peg.'),
+  nl.
+play(N,I,J,K):-
+  N>1,
+  L is N-1,
+  play(L,I,K,J),
+  play(1,I,J,_),
+  play(L,K,J,I).
+  
